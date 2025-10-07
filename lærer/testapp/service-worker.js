21a15a50f2e5a3cd5ad0d1a00d83f8dca8e8189b
Retro74 <roger.mikalsen@gmail.com>
2025-10-07 08:36:12 +0200

    publish

--- lærer/testapp/service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', event => {
-    console.log('Service Worker installert');
-});
-
-self.addEventListener('fetch', event => {
-    console.log('Henter ressurs:', event.request.url);
-});
